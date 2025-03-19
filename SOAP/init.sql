e2275e74989c5d0be277d637437a9180afbb58d9
SEGHIR <anesseghir2001@gmail.com>
2025-03-19 14:45:58 +0100

    mettre le tp soad dans un dossier

--- SOAP/init.sql
+++ SOAP/init.sql
@@ -5,6 +5,15 @@ CREATE TABLE products (
   about VARCHAR(500),
   price FLOAT
 );
+
  
 INSERT INTO products (name, about, price) VALUES
   ('My first game', 'This is an awesome game', '60')
+
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
